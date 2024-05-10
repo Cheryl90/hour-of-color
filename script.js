47842435ba3eb8c1220c70d6090c3f4a5dc65924
Cheryl90 <144183025+Cheryl90@users.noreply.github.com>
2024-05-09 21:30:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+document.getElementById('num9').addEventListener('click', function() {
+    var image = document.getElementById('blueImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
+
+document.getElementById('num11').addEventListener('click', function() {
+    var image = document.getElementById('blackImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
+
+document.getElementById('num1').addEventListener('click', function() {
+    var image = document.getElementById('yellowImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
+
+document.getElementById('num4').addEventListener('click', function() {
+    var image = document.getElementById('redImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
+
+document.getElementById('num8').addEventListener('click', function() {
+    var image = document.getElementById('orangeImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
+
+document.getElementById('num3').addEventListener('click', function() {
+    var image = document.getElementById('greyImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
+
+document.getElementById('num6').addEventListener('click', function() {
+    var image = document.getElementById('pinkImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
+
+document.getElementById('num7').addEventListener('click', function() {
+    var image = document.getElementById('purpleImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block'; 
+    } else {
+        image.style.display = 'none';  
+    }
+});
